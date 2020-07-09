31f780f880816a2e2c5af5e6988ed2e5bcec9743
sam-tripp <samantha.tripp@canada.ca>
2020-07-09 09:11:25 -0400

    Pass host url to backup script

--- src/Controller/DrmanageController.php
+++ src/Controller/DrmanageController.php
@@ -39,6 +39,7 @@ class DrmanageController {
       'bucket_location' => $conf->get('s3_bucket_location'),  // e.g. ca-central-1
       'host_base' => $conf->get('s3_host_base'),              // e.g. s3.amazonaws.com
       'host_bucket' => $conf->get('s3_host_bucket'),          // DNS-style bucket name
+      'host_url' => $host_url,
     ];
 
     // use key 'http' even if you send the request to https://...
