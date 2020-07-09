279682417ce0841accf2434847fc57cec32de5ac
sam-tripp <samantha.tripp@canada.ca>
2020-07-09 11:14:59 -0400

    Send app-name to backup.php, json error messages

--- src/Controller/DrmanageController.php
+++ src/Controller/DrmanageController.php
@@ -25,6 +25,7 @@ class DrmanageController {
   /**
    * Handle a backup request.
    * This function will contact the remote host and initiate a backup.
+   * TODO - JSON response codes
    * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
   public function request_backup() {
@@ -33,13 +34,28 @@ class DrmanageController {
     // Get AWS credentials from config
     $conf = \Drupal::config('drmanage.settings');
 
+    $nids = \Drupal::entityQuery('node')
+    ->condition('type', 'drupal_site')
+    ->condition('field_url', $host_url, '=')
+    ->execute();
+
+    if (!empty($nids)) {
+      $nid = array_shift($nids);
+      $node = \Drupal\node\Entity\Node::load($nid);
+    } else {
+      $json['messages'][] = "Unable to load node... exiting.";
+      return new JsonResponse($json);
+    }
+
+    $app_name = $node->get('field_application_name')->value;
+      
     $postdata = [
       'access_key' => $conf->get('s3_access_key'),
       'secret_key' => $conf->get('s3_secret_key'),
       'bucket_location' => $conf->get('s3_bucket_location'),  // e.g. ca-central-1
       'host_base' => $conf->get('s3_host_base'),              // e.g. s3.amazonaws.com
       'host_bucket' => $conf->get('s3_host_bucket'),          // DNS-style bucket name
-      'host_url' => $host_url,
+      'app_name' => $app_name,
     ];
 
     // use key 'http' even if you send the request to https://...
@@ -55,8 +71,31 @@ class DrmanageController {
     $result = file_get_contents("$host_url/backup.php", false, $context);
 
     if ($result === FALSE) {
-      drupal_set_message('Failed for some reason', 'error');
+      $json['messages'][] = "Backup failed... exiting.";
+      return new JsonResponse($json);
     }
+
+    // edit date of last backup
+    $t = time() - 14400;
+    $url = $node->set('field_last_backup', date('Y-m-d', $t) . 'T' . date('H:i:s', $t));
+    $node->save();
+
+    $json = json_decode($result);
+    return new JsonResponse($json);
+  }
+
+    /**
+   * Handle a restore request.
+   * This function will contact the remote host and initiate a restore.
+   * TODO - JSON response codes
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   */
+  public function request_restore() {
+    $host_url = $_POST['host_url'];
+    $backup_file = $_POST['backup_file'];
+
+    // Get AWS credentials from config
+    $conf = \Drupal::config('drmanage.settings');
 
     $nids = \Drupal::entityQuery('node')
     ->condition('type', 'drupal_site')
@@ -66,26 +105,10 @@ class DrmanageController {
     if (!empty($nids)) {
       $nid = array_shift($nids);
       $node = \Drupal\node\Entity\Node::load($nid);
-      $t = time() - 14400;
-      $url = $node->set('field_last_backup', date('Y-m-d', $t) . 'T' . date('H:i:s', $t));
-      $node->save();
+    } else {
+      $json['messages'][] = "Unable to load node... exiting.";
+      return new JsonResponse($json);
     }
-
-    $json = json_decode($result);
-    return new JsonResponse($json);
-  }
-
-    /**
-   * Handle a restore request.
-   * This function will contact the remote host and initiate a restore.
-   * @return \Symfony\Component\HttpFoundation\JsonResponse
-   */
-  public function request_restore() {
-    $host_url = $_POST['host_url'];
-    $backup_file = $_POST['backup_file'];
-
-    // Get AWS credentials from config
-    $conf = \Drupal::config('drmanage.settings');
 
     $postdata = [
       'access_key' => $conf->get('s3_access_key'),
@@ -109,21 +132,14 @@ class DrmanageController {
     $result = file_get_contents("$host_url/restore.php", false, $context);
 
     if ($result === FALSE) {
-      drupal_set_message('Failed for some reason', 'error');
+      $json['messages'][] = "Backup failed... exiting.";
+      return new JsonResponse($json);
     }
 
-    $nids = \Drupal::entityQuery('node')
-    ->condition('type', 'drupal_site')
-    ->condition('field_url', $host_url, '=')
-    ->execute();
-
-    if (!empty($nids)) {
-      $nid = array_shift($nids);
-      $node = \Drupal\node\Entity\Node::load($nid);
-      $t = time() - 14400;
-      $url = $node->set('field_last_restore', date('Y-m-d', $t) . 'T' . date('H:i:s', $t));
-      $node->save();
-    }
+    // edit date of last backup
+    $t = time() - 14400;
+    $url = $node->set('field_last_restore', date('Y-m-d', $t) . 'T' . date('H:i:s', $t));
+    $node->save();
 
     $json = json_decode($result);
     return new JsonResponse($json);
