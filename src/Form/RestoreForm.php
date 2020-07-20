a784f702426faea1a13d3ab7a8f89b80bd55eb42
sam-tripp <samantha.tripp@canada.ca>
2020-07-20 14:37:02 -0400

    Refactor getRestoreOptions

--- src/Form/RestoreForm.php
+++ src/Form/RestoreForm.php
@@ -6,6 +6,7 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\HtmlResponse;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
 
@@ -53,7 +54,7 @@ class RestoreForm extends FormBase {
             '#type' => 'radios',
             '#title' => 'Select backup to restore',
             '#default_value' => '',
-            '#options' => $this->getRestoreOptions($default_appname),
+            '#options' => $this->getRestoreOptions($default_appname, FALSE),
         );
 
         $form['response'] = [
@@ -79,11 +80,11 @@ class RestoreForm extends FormBase {
     }
 
   /**
-   * Create an options list based on the most recent backups in the S3 bucket.
+   * Update 'restore' element radio options given the selected host.
+   * TODO - JSON response codes
+   * @return 5 most recent backup file options
    */
-  private function getRestoreOptions($app_name) {
-    
-    $options = [];
+  public function getRestoreOptions($app_name = '', $onChange = TRUE) {
 
     // Get AWS credentials from config
     $conf = \Drupal::config('drmanage.settings');
@@ -96,33 +97,91 @@ class RestoreForm extends FormBase {
     putenv("AWS_ACCESS_KEY_ID=$s3_access_key");
     putenv("AWS_SECRET_ACCESS_KEY=$s3_secret_key");
 
+    // Initialize S3 client
     $s3 = new S3Client([
       'version' => 'latest',
       'region'  => $s3_bucket_location,
     ]);
-
-    // Get bucket contents
-    try {
-      $result = $s3->listObjectsV2([
-        'Bucket' => $s3_host_bucket,
-        'Prefix' => $app_name,
-      ]);
-    } catch(S3Exception $e) {
-      return $options;
-    }
-
-    // Make an options list from the last 5 items
-    $cnt = count($result['Contents']);
-    $start = $cnt > 5 ? $cnt - 5 : 0;
-    for ($n = $start; $n < $cnt; $n++) {
-      $options[$result['Contents'][$n]['Key']] = sprintf('%s (%0.2f MB)',
-        $result['Contents'][$n]['Key'],
-        $result['Contents'][$n]['Size'] / 1000000
-      );
-    }
-
-    return $options;
     
+    if ($onChange) {
+        $url = $_POST['host_url'];
+
+        $nids = \Drupal::entityQuery('node')
+            ->condition('type', 'drupal_site')
+            ->condition('status', NODE_PUBLISHED)
+            ->condition('field_url', $url, '=')
+            ->execute();
+
+        if(!empty($nids)){
+            $nid = array_shift($nids);
+            $node = \Drupal\node\Entity\Node::load($nid);
+        } else {
+            $json['html'][] = "<div><p>Unable to load node... exiting.</p></div>";
+            return new JsonResponse($json);
+        }
+        $app_name = $node->get('field_application_name')->value;
+
+        // Get bucket contents in app_name directory
+        try {
+            $result = $s3->listObjectsV2([
+              'Bucket' => $s3_host_bucket,
+              'Prefix' => $app_name,
+            ]);
+        } catch(S3Exception $e) {
+            $json['html'][] = "<div><p>listObjectsV2 error... exiting.</p></div>";
+            return new JsonResponse($json);
+        }
+
+        // Make an options list from the last 5 items
+        $cnt = count($result['Contents']);
+        $start = $cnt > 5 ? $cnt - 5 : 0;
+        for ($n = $start; $n < $cnt; $n++) {
+
+        // Format the selector options in drupalized html
+        $patterns = array('/', '.tar.gz');
+        $replacements = array('', 'targz');
+
+        $filename = preg_replace($patterns, $replacements, $results['Contents'][$n]['Key']);
+
+        $label = sprintf('%s (%0.1f MB)',
+        $result['Contents'][$n]['Key'],
+        $result['Contents'][$n]['Size'] / 1000000);
+        
+        $html =  '<div class="js-form-item form-item js-form-type-radio form-type--radio form-type--boolean js-form-item-restore form-item--restore">';
+        $html .= '<input data-drupal-selector="edit-restore-' . $filename . '" ';
+        $html .= 'type="radio" id="edit-restore-' . $filename . '" name="restore" ';
+        $html .= 'value="' . $results['Contents'][$n]['Key'] . '"';
+        $html .= 'class="form-radio form-boolean form-boolean--type-radio" tabindex="-1">';
+        $html .= '<label for="edit-restore-' . $filename . '" ';
+        $html .= 'class="form-item_label option">' . $label . '</label></div>';
+        $json['html'][] = $html;
+        }
+        return new JsonResponse($json);
+
+    } else {
+        $options = [];
+
+        // Get bucket contents in app_name directory
+        try {
+            $result = $s3->listObjectsV2([
+            'Bucket' => $s3_host_bucket,
+            'Prefix' => $app_name,
+            ]);
+        } catch(S3Exception $e) {
+            return $options;
+        }
+
+        // Make an options list from the last 5 items
+        $cnt = count($result['Contents']);
+        $start = $cnt > 5 ? $cnt - 5 : 0;
+        for ($n = $start; $n < $cnt; $n++) {
+        $options[$result['Contents'][$n]['Key']] = sprintf('%s (%0.2f MB)',
+            $result['Contents'][$n]['Key'],
+            $result['Contents'][$n]['Size'] / 1000000
+        );
+        }  
+        return $options;
+    } 
   }
 
   public function validateForm(array &$form, FormStateInterface $form_state) {
@@ -130,5 +189,4 @@ class RestoreForm extends FormBase {
 
   public function submitForm(array &$form, FormStateInterface $form_state){
   }
-
 }
